81db99eb5bd227822c2aa99bc0ef54ce17f85b26
MichelFIX <MichelFIX@DESKTOP-S1BRKLP>
2016-12-17 12:17:07 +0100

    Commit and push by Michel

--- src/me/mickerd/pc/core/main/main.java
+++ src/me/mickerd/pc/core/main/main.java
@@ -1,5 +1,20 @@
 package me.mickerd.pc.core.main;
 
-public class main {
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class main extends JavaPlugin {
+	
+    static Plugin pl = null;
+	
+	public void onEnable(){
+		pl = this;
+	    // Commands
+		getCommand("pc").setExecutor(new PCCommando());
+	}
+	
+	public void onDisable(){
+		pl = null;
+	}
 
 }
