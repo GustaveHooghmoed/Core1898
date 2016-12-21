2b479b5152a39020ca03a0a1b8c2970e141ff261
Digibord <Digibord@L300SRV-01.michaelschool.local>
2016-12-21 12:11:45 +0100

    rm

--- src1/me/mickerd/pardoes/core/main/main.java
+++ src1/me/mickerd/pardoes/core/main/main.java
@@ -1,7 +1,21 @@
 package me.mickerd.pardoes.core.main;
 
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class main extends JavaPlugin {
+	
+	static Plugin main = null;
+	public void onEnable(){
+		main = this;
+	}
+	
+	public void onDisable(){
+		main = null;
+	}
 
+	public Plugin rm() {
+		return main;
+	}
+	
 }
