9723eeb739e5e1e7ac3c0a5bd291b05187d012d4
Digibord <Digibord@L300SRV-01.michaelschool.local>
2016-12-21 12:14:32 +0100

    Command 1

--- src1/me/mickerd/pardoes/core/main/main.java
+++ src1/me/mickerd/pardoes/core/main/main.java
@@ -1,5 +1,7 @@
 package me.mickerd.pardoes.core.main;
 
+import me.mickerd.pardoes.core.commands.PC;
+
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -8,8 +10,15 @@ public class main extends JavaPlugin {
 	static Plugin main = null;
 	public void onEnable(){
 		main = this;
+		getCommands();
 	}
 	
+	private void getCommands() {
+		
+		getCommand("pc").setExecutor(new PC());
+		
+	}
+
 	public void onDisable(){
 		main = null;
 	}
