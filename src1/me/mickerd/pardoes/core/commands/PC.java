8994e047f6f2e433390326ad763df91c102cad68
Digibord <Digibord@L300SRV-01.michaelschool.local>
2016-12-21 12:15:30 +0100

    xd

---
+++ src1/me/mickerd/pardoes/core/commands/PC.java
@@ -0,0 +1,16 @@
+package me.mickerd.pardoes.core.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class PC implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command label, String cmnd,
+			String[] args) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
